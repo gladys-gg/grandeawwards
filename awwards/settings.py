e1b6b28992710b520b0c7c053c6d7dac2aa6009d
Gladys Mwangi <gladysmwangi04@yahoo.com>
2022-06-11 14:36:22 +0300

    installed cloudinary

--- awwards/settings.py
+++ awwards/settings.py
@@ -11,6 +11,9 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+import cloudinary
+import cloudinary.uploader
+import cloudinary.api
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -41,6 +44,7 @@ INSTALLED_APPS = [
     #own
     'grandawards',
     'rest_framework',
+    'cloudinary',
 ]
 
 MIDDLEWARE = [
