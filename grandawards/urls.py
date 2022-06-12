0dd7937d92bc0855189e0dcbe13528dd188a2147
Gladys Mwangi <gladysmwangi04@yahoo.com>
2022-06-13 02:12:57 +0300

    Added the profile and project path

--- grandawards/urls.py
+++ grandawards/urls.py
@@ -10,11 +10,17 @@ urlpatterns = [
     path('',views.index, name='index'),
     #profile paths
     path('profiles/',views.profile_list),
+        # Profile Section
+    path('profile/<int:user_id>', views.UserProfile, name='profile'),
+    path('profile/edit', views.EditProfile, name="editprofile"),
     # User Authentication
     path('profiles/<int:id>', views.profile_detail),
     path('sign-up/',views.register,name='sign-up'),
     path('accounts/sign-in/',views.signin,name='sign-in'),
     path('sign-out/', views.signout, name='sign-out'),
+    
+    #project
+    path('newproject', views.NewProject, name='newproject'),
 
 ]
 
