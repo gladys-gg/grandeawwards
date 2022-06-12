edc6d8164f18206d97ff99310095d9bc21c1d421
Gladys Mwangi <gladysmwangi04@yahoo.com>
2022-06-12 23:32:21 +0300

    api profile detail path

--- grandawards/urls.py
+++ grandawards/urls.py
@@ -4,14 +4,18 @@ from django.contrib.auth import views as auth_views
 from django.urls import reverse_lazy
 from django.conf import settings
 from django.conf.urls.static import static
+from rest_framework.urlpatterns import format_suffix_patterns
 
 urlpatterns = [
     path('',views.index, name='index'),
+    #profile paths
     path('profiles/',views.profile_list),
     # User Authentication
-
+    path('profiles/<int:id>', views.profile_detail),
     path('sign-up/',views.register,name='sign-up'),
     path('accounts/sign-in/',views.signin,name='sign-in'),
     path('sign-out/', views.signout, name='sign-out'),
 
 ]
+
+urlpatterns=format_suffix_patterns(urlpatterns)
