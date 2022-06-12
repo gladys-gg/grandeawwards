9b81ed80aa50ca13c81dc96db172ca6c1d03a92b
Gladys Mwangi <gladysmwangi04@yahoo.com>
2022-06-12 08:00:58 +0300

    Edited the class project and return string

--- grandawards/models.py
+++ grandawards/models.py
@@ -13,16 +13,18 @@ class Profile(models.Model):
         self.save()
 
     def __str__(self):
-        return self.name
+        return self.fullname
     
 class Project(models.Model):
+    title= models.CharField(max_length=100)
     project_image= CloudinaryField('image')
     profile = models.ForeignKey(Profile,on_delete=models.CASCADE, null=True)
-    name= models.CharField(max_length=100)
     description= models.TextField()
     link= models.CharField(max_length=250)
 
-
+    def __str__(self):
+        return self.title + '' + self.description
+    
     def save_project(self):
         self.save()
 
