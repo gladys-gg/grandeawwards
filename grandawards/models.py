3bd64ce4533bbe9bd4ee24e1c28251d8da4db195
Gladys Mwangi <gladysmwangi04@yahoo.com>
2022-06-12 05:17:39 +0300

    created the project class

--- grandawards/models.py
+++ grandawards/models.py
@@ -14,3 +14,24 @@ class Profile(models.Model):
 
     def __str__(self):
         return self.name
+    
+class Project(models.Model):
+    project_image= CloudinaryField('image')
+    profile = models.ForeignKey(Profile,on_delete=models.CASCADE, null=True)
+    name= models.CharField(max_length=100)
+    description= models.TextField()
+    link= models.CharField(max_length=250)
+
+
+    def save_project(self):
+        self.save()
+
+
+    @classmethod
+    def search_by_name(cls,search_term):
+        '''
+        method to search projects based on name
+        '''
+        projects=cls.objects.filter(name__icontains=search_term)
+
+        return projects
