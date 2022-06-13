450d025787f2fdfe24245125cfede445724db55e
Gladys Mwangi <gladysmwangi04@yahoo.com>
2022-06-13 13:21:15 +0300

    created the project serializer

--- grandawards/serializers.py
+++ grandawards/serializers.py
@@ -1,7 +1,12 @@
 from rest_framework import serializers
-from .models import Profile
+from .models import Profile, Project
 
 class ProfileSerializer(serializers.ModelSerializer):
     class Meta:
         model=Profile
-        fields=('fullname','bio','profile_pic','user')
+        fields=('fullname','bio','profile_pic','location','user','url')
+        
+class ProjectSerializer(serializers.ModelSerializer):
+    class Meta:
+        model=Project
+        fields=('title','project_image','profile','description','link')
